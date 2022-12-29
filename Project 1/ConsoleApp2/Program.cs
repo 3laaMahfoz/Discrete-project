045b2e7c1da654d1993ad51c6ca34438f4ffd659
3laaMahfoz <121312395+3laaMahfoz@users.noreply.github.com>
2022-12-29 22:46:23 +0200

    Add files via upload

---
+++ Project 1/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// find prime number
+ Console.WriteLine("please enter the start number :");
+
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("please enter the end number :");
+
+int end = int.Parse(Console.ReadLine());
+
+ Boolean f = true;
+
+for (int i = first; i < end; i++)
+{
+    for (int j = 2; j < i; j++) 
+    {
+        if (i % j == 0)
+        {
+            f = false;
+            break;
+        }
+        else
+        {
+            f = true;
+            continue;
+        }
+    }
+    if (f == true)
+        Console.WriteLine("the prime number is for your range is " );
+    Console.WriteLine(i);
+}
+
---
+++ Project 2/ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //find perfect number
+                int n, i, sum;
+                int min, max;
+                Console.Write("enter start number : ");
+                min = Convert.ToInt32(Console.ReadLine());
+                Console.Write("enter end number : ");
+                max = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The Perfect numbers: ");
+                for (n = min; n <= max; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == n)
+                        Console.Write("{0} ", n);
+                }
+              
+            }
+        }
+    }
+    
+
